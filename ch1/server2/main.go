d01a5b2bb36f7b44a4f6930254c9c1ba618db149
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-30 16:36:40 +0900

    Fix ch1 server2

--- ch1/server2/main.go
+++ ch1/server2/main.go
@@ -18,15 +18,15 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    # 同時書き込み防止のためにLockをかける
+	// 同時書き込み防止のためにLockをかける
 	mu.Lock()
 	count++
 	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q¥n", r.URL.Path)
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
 func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
-	fmt.Fprintf(w, "Count %d¥n", count)
+	fmt.Fprintf(w, "Count %d\n", count)
 	mu.Unlock()
 }
