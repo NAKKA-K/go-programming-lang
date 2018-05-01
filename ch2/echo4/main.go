9bdb9ff8d4537f1846b80c6bcfa80d344768669a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-01 21:33:06 +0900

    Add ch2 echo4

---
+++ ch2/echo4/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	// non return, when n exist.
+	if !*n {
+		fmt.Println()
+	}
+}
