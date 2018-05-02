f725fa5dbb213a903c90b97773996c9baf824c4e
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-02 19:19:21 +0900

    Add ch2 tempconv Kelvin

--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -14,4 +14,4 @@ const (
 )
 
 func (c Celsius) String() string    { return fmt.Sprintf("%gC", c) }
-func (f Fahrenheit) String() string { return fmt.Sprintf("%gC", f) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%gF", f) }
