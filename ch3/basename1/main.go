f4c7c8d3ddec82d05e6fc29b9f397d9916607f9a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-04 14:51:26 +0900

    Add ch3 basename1

---
+++ ch3/basename1/main.go
@@ -0,0 +1,19 @@
+package basename1
+
+func basename(s string) string {
+	// Delete last '/' and before all.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
