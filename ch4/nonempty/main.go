dad94b12e348880c0d899fd4a73b430f5c163a31
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-06 16:58:11 +0900

    Add ch4 nonempty

---
+++ ch4/nonempty/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
+
+func main() {
+	fmt.Println(nonempty([]string{"", "aaa", "age"}))
+}
