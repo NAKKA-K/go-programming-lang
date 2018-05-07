14fc416446e442648de4ae6dc867a0ed8e8d4dfb
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-07 09:37:56 +0900

    Add stack remove(pop)

--- ch4/nonempty3/main.go
+++ ch4/nonempty3/main.go
@@ -12,6 +12,11 @@ func nonempty(strings []string) []string {
 	return out
 }
 
+func remove(slice []string, i int) []string {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
+}
+
 func main() {
 	fmt.Println(nonempty([]string{"", "aaa", "age"}))
 }
