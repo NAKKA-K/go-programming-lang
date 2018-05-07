6130ba5ea6373dd4302e834d2f559bca489bccb0
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-07 10:09:57 +0900

    Add ch4 practice3

---
+++ ch4/practice3/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func reverse(s *[3]int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	slice := [3]int{1, 2, 3}
+	reverse(&slice)
+	fmt.Println(slice)
+}
