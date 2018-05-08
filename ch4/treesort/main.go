a3cd6cda9973333c0751fc26a61180289b28796a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-05-08 21:45:27 +0900

    Add ch4 treesort

---
+++ ch4/treesort/main.go
@@ -0,0 +1,39 @@
+package treesort
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// values内の値をその中でソートする
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// tの要素をvaluesの正しい順序に追加し結果のスライスを返します
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
